8117b72701ce614d7928a22ac123a6bf8e61969d
wangkq <908891024@qq.com>
2018-06-20 10:16:23 +0800

    add Default .h

--- PoporUI/Classes/PoporUI.h
+++ PoporUI/Classes/PoporUI.h
@@ -16,4 +16,59 @@ FOUNDATION_EXPORT const unsigned char UIExtensionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <UIExtension/PublicHeader.h>
 
+// BlockView
+#import "BlockActionSheet.h"
+#import "BlockAlertView.h"
+
+// IToast
+#import "IToastKeyboard.h"
+
+// Responder
+#import "UIResponder+Router.h"
+
+// SimpleWKWebVC
+#import "SimpleWKWebVC.h"
+
+// Tool
+#import "DeviceDisk.h"
+#import "NSFileManager+Tool.h"
+#import "UIDevice+Permission.h"
+#import "UIDevice+SaveImage.h"
+#import "UIDevice+Tool.h"
+#import "UIPasteImageTextView.h"
+
+// UIButton
+#import "UIButton+CommonStyle.h"
+#import "UIButton+UIEdgeInsets.h"
+
+// UIImage
+#import "UIImage+Tool.h"
+
+// UILabel
+#import "UILabel+Size.h"
+#import "UILabelInsets.h"
+#import "UILabelPhone.h"
+
+// UIScrollView
+#import "AllowPanGRPassSV.h"
+#import "UIScrollView+tool.h"
+
+// UITextField
+#import "UIInsetsTextField.h"
+#import "UITextField+MaxLength.h"
+
+// UITextView
+#import "UIPlaceHolderTextView.h"
+#import "UITextView+MaxLength.h"
+#import "UITextView+Size.h"
+
+
+// UIView
 #import "UIView+Extension.h"
+#import "UIView+Tool.h"
+
+// UIViewController
+//#import "UIViewController+AC.h"
+#import "UIViewController+LeeBase.h"
+#import "UIViewController+ncBar.h"
+#import "UIViewController+TapEndEdit.h"
